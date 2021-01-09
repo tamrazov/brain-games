b619b03d1f0048c03a94aa4afd4acddfd92982da
Alex <box-570@yandex.ru>
2021-01-09 17:45:25 +0300

    fix bags

--- src/games/Calc.php
+++ src/games/Calc.php
@@ -9,13 +9,13 @@ const DESCRIPTION = 'What is the result of the expression?';
 
 function run()
 {
-    $getGameData = function () {
+    $getGameData = function (): array {
         $num1 = rand(1, 10);
         $num2 = rand(1, 10);
         $arrSymbols = array("+", "-", "*");
         $randomSymbol = $arrSymbols[array_rand($arrSymbols, 1)];
         $question = "{$num1} {$randomSymbol} {$num2}";
-        $answer = calc($num1, $num2, $randomSymbol);
+        $answer = strval(calc($num1, $num2, $randomSymbol));
 
         return array(
             "question" => (string) $question,
